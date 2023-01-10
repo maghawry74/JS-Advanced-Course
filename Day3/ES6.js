b8193d8177e360037a0555354abc15209cec35cf
maghawry74 <maghawry.hussein20@gmail.com>
2023-01-10 13:00:27 +0200

    Sperated the code into two files

---
+++ Day3/ES6.js
@@ -0,0 +1,54 @@
+// Using ES6 Syntaic
+class Shape {
+  constructor(width, height) {
+    this.width = width
+    this.height = height
+  }
+  toString() {
+    return `Width: ${this.width} || Height: ${
+      this.height
+    } || Area: ${this.Area()} Preimter: ${this.Premiter()}`
+  }
+  Area() {
+    return this.width * this.height
+  }
+  Premiter() {
+    return 2 * (this.width + this.height)
+  }
+}
+
+class Rectangle extends Shape {
+  constructor(width, height) {
+    super(width, height)
+  }
+}
+class Square extends Shape {
+  constructor(para) {
+    super(para, para)
+  }
+}
+class Circle extends Shape {
+  static objectCounter = 0
+  constructor(para) {
+    super(para, para)
+    Circle.objectCounter++
+  }
+  Area() {
+    return Math.round(Math.PI * this.width * this.height)
+  }
+  Premiter() {
+    return 2 * (this.width + this.height)
+  }
+}
+Shape.prototype.test = function () {
+  return "this is a test function"
+}
+var Rec1 = new Rectangle(5, 6)
+var Sqr1 = new Square(5)
+var Cir1 = new Circle(6)
+var Cir2 = new Circle(8)
+
+console.log(Rec1.toString())
+console.log(Sqr1.toString())
+console.log(Cir1.toString())
+console.log(Circle.objectCounter)
---
+++ Day3/Native Js.js
@@ -0,0 +1,52 @@
+// Using Native JS
+function Shape(width, height) {
+  this.width = width
+  this.height = height
+}
+Shape.prototype.Area = function () {
+  return this.width * this.height
+}
+Shape.prototype.Premiter = function () {
+  return 2 * (this.width + this.height)
+}
+Shape.prototype.toString = function () {
+  return `Width: ${this.width} || Height: ${
+    this.height
+  } || Area: ${this.Area()} Preimter: ${this.Premiter()}`
+}
+
+function Rectangle(width, height) {
+  Shape.call(this, width, height)
+}
+Rectangle.prototype = Object.create(Shape.prototype)
+
+function Square(para) {
+  Shape.call(this, para, para)
+}
+Square.prototype = Object.create(Shape.prototype)
+
+function Circle(para) {
+  if (Circle.objectCounter == undefined) {
+    Circle.objectCounter = 1
+  } else {
+    Circle.objectCounter++
+  }
+  Shape.call(this, para, para)
+}
+Circle.prototype = Object.create(Shape.prototype)
+Circle.prototype.Area = function () {
+  return Math.round(Math.PI * this.width * this.height)
+}
+Circle.prototype.Premiter = function () {
+  return Math.round(2 * Math.PI * this.width)
+}
+
+var Rec1 = new Rectangle(5, 6)
+var Sqr1 = new Square(5)
+var Cir1 = new Circle(6)
+var Cir2 = new Circle(8)
+
+console.log(Rec1.toString())
+console.log(Sqr1.toString())
+console.log(Cir1.toString())
+console.log(Circle.objectCounter)
