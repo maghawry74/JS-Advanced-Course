541fe66a2ec09184ad662d9a1333714c4264bb02
maghawry74 <maghawry.hussein20@gmail.com>
2023-01-13 15:03:26 +0200

    handling error

--- Day5/demo.js
+++ Day5/demo.js
@@ -12,6 +12,7 @@ function courseDisplay(course) {
 
 // Task 2
 // 1- using Fetch --> then --> catch
+
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => {
     return response.json()
@@ -28,14 +29,19 @@ fetch("https://jsonplaceholder.typicode.com/users")
     })
   })
   .catch((error) => {
-    console.log(error)
+    document.querySelector(".tabs").innerHTML =
+      "Error Occured While fetching Users"
   })
 
 function fetchPosts(e) {
-  document.querySelectorAll(".user").forEach((button) => button.classList.remove("checked"))
+  document
+    .querySelectorAll(".user")
+    .forEach((button) => button.classList.remove("checked"))
   e.target.classList.add("checked")
 
-  fetch(`https://jsonplaceholder.typicode.com/posts?userId=${e.target.dataset.id}`)
+  fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${e.target.dataset.id}`
+  )
     .then((respose) => respose.json())
     .then((data) => {
       let displaySection = document.querySelector(".display")
@@ -44,33 +50,52 @@ function fetchPosts(e) {
         displaySection.innerHTML += `<h3>${post.title}</h3>`
       })
     })
+    .catch((error) => {
+      document.querySelector(
+        ".display"
+      ).innerHTML = `Error Occured while fetching Posts`
+    })
 }
 
 // 2- using Async Await
 
 async function getUsers() {
-  let response = await fetch("https://jsonplaceholder.typicode.com/users")
-  let users = await response.json()
-  let usersSection = document.querySelector(".tabs")
-  users.forEach((user) => {
-    usersSection.innerHTML += `<button class="user" data-id=${user.id}>${user.name}</button>`
-  })
-  document.querySelectorAll(".user").forEach((item) => {
-    item.addEventListener("click", (e) => {
-      fetchPosts(e)
+  try {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users")
+    let users = await response.json()
+    let usersSection = document.querySelector(".tabs")
+    users.forEach((user) => {
+      usersSection.innerHTML += `<button class="user" data-id=${user.id}>${user.name}</button>`
     })
-  })
+    document.querySelectorAll(".user").forEach((item) => {
+      item.addEventListener("click", (e) => {
+        fetchPosts(e)
+      })
+    })
+  } catch (error) {
+    document.querySelector(".tabs").innerHTML =
+      "Error Occured While fetching Users"
+  }
 }
 
 async function fetchPosts(e) {
-  document.querySelectorAll(".user").forEach((button) => button.classList.remove("checked"))
+  document
+    .querySelectorAll(".user")
+    .forEach((button) => button.classList.remove("checked"))
   e.target.classList.add("checked")
-
-  let response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${e.target.dataset.id}`)
-  let userPosts=await response.json()
-  let displaySection = document.querySelector(".display")
-  displaySection.innerHTML = ""
-  userPosts.forEach((post) => {
-    displaySection.innerHTML += `<h3>${post.title}</h3>`
-  })
+  try {
+    let response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?userId=${e.target.dataset.id}`
+    )
+    let userPosts = await response.json()
+    let displaySection = document.querySelector(".display")
+    displaySection.innerHTML = ""
+    userPosts.forEach((post) => {
+      displaySection.innerHTML += `<h3>${post.title}</h3>`
+    })
+  } catch (error) {
+    document.querySelector(
+      ".display"
+    ).innerHTML = `Error Occured while fetching Posts`
+  }
 }
